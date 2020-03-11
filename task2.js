d1c8d5308b00c0ec0740ddeaefd0d2f7247f2319
IvanPustovit <pustovit.for@gmail.com>
2020-03-11 20:37:28 +0200

    Homework11_promis

---
+++ task2.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const users = [
+    { name: 'Mango', active: true },
+    { name: 'Poly', active: false },
+    { name: 'Ajax', active: true },
+    { name: 'Lux', active: false },
+  ];
+  
+  //   const toggleUserState = (allUsers, userName, callback) => {
+  //   const updatedUsers = allUsers.map(user =>
+  //   user.name === userName ? { ...user, active: !user.active } : user,
+  //   );
+  
+  //   callback(updatedUsers);
+  // };
+
+  
+  
+    const logger = updatedUsers => console.table(updatedUsers);
+  
+  /*
+   * Сейчас работает так
+   */
+    // toggleUserState(users, 'Mango', logger);
+    // toggleUserState(users, 'Lux', logger);
+
+    // console.log(toggleUserState);
+  
+  /*
+   * Должно работать так
+   */
+
+  const toggleUserState = (allUsers, userName) =>{
+    return new Promise((resolve, reject) => {
+      const updatedUsers = allUsers.map(user =>
+          user.name === userName ? { ...user, active: !user.active } : user,
+          );  
+
+          resolve(updatedUsers)
+    })
+  }
+    toggleUserState(users, 'Mango').then(logger);
+    toggleUserState(users, 'Lux').then(logger);
